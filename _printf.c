b322e0e88973bb50cb121d2d2fda3e4f5d0ec216
Anas Asimi <anas2020cr7@gmail.com>
2023-09-16 21:50:14 +0100

    commit

--- _printf.c
+++ _printf.c
@@ -1,17 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 
-	/**
-	 * _printf - Printf function
-	 * @format: format.
-	 * Return: Number of printed chars.
-	 */
-	int _printf(const char *format, ...)
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Number of printed chars.
+ */
+int _printf(const char *format, ...)
 {
 	int i = 0;
+	int characters_printed = 0;
 
 	while (format[i])
-		putchar(format[i++]);
+	{
+		putchar(format[i]);
+		i++;
+		characters_printed++;
+	}
 
 	return (1);
 }
