96d3d012003f2c5bed34ba6017006413616c78c4
Anas Asimi <anas2020cr7@gmail.com>
2023-09-16 21:47:49 +0100

    print without formating

--- _printf.c
+++ _printf.c
@@ -1,12 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Number of printed chars.
- */
-int _printf(const char *format, ...)
+	/**
+	 * _printf - Printf function
+	 * @format: format.
+	 * Return: Number of printed chars.
+	 */
+	int _printf(const char *format, ...)
 {
-	format = format;
+	int i = 0;
+
+	while (format[i])
+		putchar(format[i++]);
+
 	return (1);
 }
