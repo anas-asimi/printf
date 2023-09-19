8f93d0d30e492ea83a6d415f07a8778e5a9a8d41
Anas Asimi <anas2020cr7@gmail.com>
2023-09-19 21:11:20 +0100

    add %i

--- _print_specifier.c
+++ _print_specifier.c
@@ -28,7 +28,7 @@ int _print_specifier(char *specifier, va_list *list)
 		return (print_character(va_arg(*list, int)));
 	if (type == 's')
 		return (print_string(va_arg(*list, char *)));
-	if (type == 'd')
+	if (type == 'd' || type == 'i')
 		return (print_number(va_arg(*list, int)));
 	return (1);
 }
--- test/_printf_test.c
+++ test/_printf_test.c
@@ -19,31 +19,31 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	len = _printf("Character:[%c]\n", 'H');
 	len2 = printf("Character:[%c]\n", 'H');
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	len = _printf("Percent :[%%]\n");
 	len2 = printf("Percent :[%%]\n");
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	len = _printf("String:[%s]\n", "I am a string !");
 	len2 = printf("String:[%s]\n", "I am a string !");
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	len = _printf("Multiple string: [%s] [%s]\n", "I am a string 1!", "I am a string 2!");
 	len2 = printf("Multiple string: [%s] [%s]\n", "I am a string 1!", "I am a string 2!");
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	len = _printf("Multiple types: [%s] [%c]\n", "I am a string 1!", 'Z');
 	len2 = printf("Multiple types: [%s] [%c]\n", "I am a string 1!", 'Z');
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	len = _printf("Numbers: [%d]\n", 999);
 	len2 = printf("Numbers: [%d]\n", 999);
-	printf("printed characters are : [%d] - [%d]\n", len, len2);
+	printf("printed characters are : [%d] - [%d]\n\n", len, len2);
 
 	return (0);
 	_printf("Length:[%d, %i]\n", len, len);
