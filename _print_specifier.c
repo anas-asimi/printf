3b2d9df7a8e5c58397f8ae07d2455b291939978d
Anas Asimi <anas2020cr7@gmail.com>
2023-09-19 22:17:21 +0100

    commit

--- _print_specifier.c
+++ _print_specifier.c
@@ -28,7 +28,7 @@ int _print_specifier(char *specifier, va_list *list)
 		return (print_character(va_arg(*list, int)));
 	if (type == 's')
 		return (print_string(va_arg(*list, char *)));
-	if (type == 'd' || type == 'i')
+	if (type == 'd' || type == 'i' || type == 'u')
 		return (print_number(va_arg(*list, int)));
 	if (type == 'b')
 		return (print_binary(va_arg(*list, int)));
